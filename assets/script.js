7224e35f8945f8450a3c5e2e5eea8ce4d72a2109
Shubham Vishwakarma <4264954-Shubhamvis98@users.noreply.gitlab.com>
2022-08-25 00:33:15 +0530

    updated

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-function show(id, sec=0) {
-	setTimeout(function(){
-		document.getElementById(id).style.display = 'block';
-	}, sec*1000);
-}
-
-show("jack", 0.5)
-show("subtxt", 2.5)
--- repo/README.md
+++
@@ -1,3 +0,0 @@
-# [<u>shubhamvis98.github.io</u>](https://shubhamvis98.github.io)
-
-### Created by [<u>Gitfolio</u>](https://github.com/imfunniee/gitfolio)
